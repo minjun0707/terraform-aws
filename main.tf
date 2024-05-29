6b9855506c55c1035ae4dd51e8c60cae5598f91e
ocean(김민준) <>
2024-05-29 11:05:54 +0900

    delete terraform cloud info

--- main.tf
+++ main.tf
@@ -1,25 +1,9 @@
-terraform {
-  cloud {
-    organization = "minjun0707-terraform"
-    workspaces {
-      name = "terraform-aws"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
 provider "aws" {
   region = "ap-northeast-2"
 }
 
 resource "aws_s3_bucket" "aws_s3_bucket" {
-  bucket = "terrafrom-bucket" # 원하는 버킷 이름으로 변경
+  bucket = "terrafrom-bucket"
 
   tags = {
     Environment = "dev"

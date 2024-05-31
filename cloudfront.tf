485835889289444369b2fc58d1f7e95259258c0f
ocean(김민준) <75360915+minjun0707@users.noreply.github.com>
2024-05-31 10:12:11 +0900

    add cloudfront

---
+++ cloudfront.tf
@@ -0,0 +1,58 @@
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.www_bucket.bucket_domain_name
+    origin_id   = aws_s3_bucket.www_bucket.id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+
+    
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  comment             = ""
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.www_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  # 지리적 제한: 특정 국가에서만 접근하도록 화이트리스트 작성
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      # locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
